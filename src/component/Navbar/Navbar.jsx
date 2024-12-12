69b6ca39eaa5a6664af2b7720f1719c25ae4cbd8
MD Yasin Sharif Nayeem <islamnayeem699@gmail.com>
2024-12-12 11:50:17 +0600

    update navbar menu

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -9,21 +9,11 @@ import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
 
-const[show,setShow]=useState(false)
-const tapshow=()=>{
-  setShow(!show)
-}
-useEffect(()=>{
-function resize() {
-  if(window.innerwidth<700){
-    setShow(false)
-  }else{
-    setShow(true)
+  const[show,setShow]=useState(false)
+  const tapshow=()=>{
+    setShow(!show)
   }
-}
-resize()
-window.addEventListener("resize",resize)
-},[])
+
 
   return (
     <>
@@ -32,10 +22,7 @@ window.addEventListener("resize",resize)
         <div className="menu  ">
           <div className="logo  ">
            <img className='cursor-pointer  ' src="Logo.png" alt="logo.png" />
-           
-          {
-            show &&
-            <ul className=" ">
+           <ul   className="lg:flex hidden ">
             <li><Link className="text-lg font-medium font-Railway   hover:text-green-600 text-black" to="#">Home</Link></li>
             <li><Link className="text-lg font-medium font-Railway  hover:text-green-600 text-black" to="#">About</Link></li>
             <li><Link className="text-lg font-medium font-Railway  hover:text-green-600 text-black" to="#">Portfolio</Link></li>
@@ -47,13 +34,30 @@ window.addEventListener("resize",resize)
               Book a Table
             </button>
             </div>
+            
+          </ul>
+          {
+            show &&
+            <ul   className=" ">
+            <li><Link className="text-lg font-medium font-Railway   hover:text-green-600 text-black" to="#">Home</Link></li>
+            <li><Link className="text-lg font-medium font-Railway  hover:text-green-600 text-black" to="#">About</Link></li>
+            <li><Link className="text-lg font-medium font-Railway  hover:text-green-600 text-black" to="#">Portfolio</Link></li>
+            <li><Link className="text-lg font-medium font-Railway  hover:text-green-600 text-black" to="#">Clients</Link></li>
+            <li><Link className="text-lg font-medium font-Railway  hover:text-green-600 text-black" to="#">Blog</Link></li>
+            <li><Link className="text-lg font-medium font-Railway  hover:text-green-600 text-black" to="#">Contact</Link></li>
+            <div className="buttonnav lg:items-center">
+            <button className="bg-[#FEBF00] font-bold text-[#0A1425] px-[25px] py-[10px] font-Roboto text-[16px] ml-0 lg:ml-[292px]  hover:text-green-600 lg:rounded-none rounded-[10px] ">
+              Book a Table
+            </button>
+            </div>
+            
           </ul>
                
           }
 
            
           
-<FaBars onClick={tapshow} className="lg:hidden block cursor-pointer text-white relative" />
+<FaBars onClick={tapshow} className="lg:hidden block cursor-pointer text-white relative z-20" />
           
           </div>
           
